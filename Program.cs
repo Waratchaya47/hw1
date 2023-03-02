1fe299e80deeb294e46bac3265020af53d582560
kiki <waratchaya.rasr@mail.kmutt.ac.th>
2023-03-02 15:09:27 +0700

    ciaonepass

--- Program.cs
+++ Program.cs
@@ -13,11 +13,37 @@ namespace hw {
                   int p5 = 0;
                   int p4 = 0;
                   int p3 = 0;
-                  int n = 0;
                   int p2 = 0;
+                  int p1 = 0;
+                  int n = 0;
                   
                      switch(a) {
                         case "CIA": 
+                          int x6c = (int)pass / 100000;
+                          p6 = pass - (x6c * 100000);
+                          int x5c = (int)p6 / 10000;
+                          p5 = p6 - (x5c * 10000);
+                          int x4c = (int)p5 / 1000;
+                          p4 = p5 - (x4c * 1000);
+                          int x3c = (int)p4 / 100;
+                          p3 = p4 - (x3c * 100);
+                          int x2c = (int)p3 / 10;
+                          p2 = p3 - (x2c * 10);
+                          if(p2 % 3 == 0) {
+                            int x6c2 = (int)pass / 100000;
+                            p6 = pass - (x6c2 * 100000);
+                            int x5c2 = (int)p6 / 10000;
+                            p5 = p6 - (x5c2 * 10000);
+                            int x4c2 = (int)p5 / 1000;
+                            p4 = p5 - (x4c2 * 1000);
+                            int x3c2 = (int)p4 / 100;
+                            p3 = p4 - (x3c2 * 100);
+                            int x2c2 = (int)p3 / 10;
+                            p2 = (int)x2c2;
+                            if(p2 == 0) {
+                              
+                            }
+                          }
 
                           
                           break;
@@ -27,7 +53,7 @@ namespace hw {
                           if(p6 > 3 && p6 < 8) {
                             p6 = pass - (x6f * 100000);
                             int x5f = (int)p6 / 10000;
-                            p5 = pass - (x5f * 10000);
+                            p5 = p6 - (x5f * 10000);
                             int x4f = (int)p5 / 1000;
                             p4 = p5 - (x4f * 1000);
                             int x3f = (int)p4 / 100;
